fa621f81506c126969f05e044e557b073f0241bc
mosama88 <mosama88@hotmail.com>
2024-04-21 13:59:13 +0200

    delete select 2

--- resources/views/dashboard/doctors/delete_select.blade.php
+++ resources/views/dashboard/doctors/delete_select.blade.php
@@ -1,14 +1,13 @@
 <!-- Modal -->
 <div class="modal fade" id="delete_select" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">
                     حذف مجموعة أطباء</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
             </div>
             <form action="{{ route('dashboard.doctors.destroy', 'test') }}" method="post">
                 @method('DELETE')
