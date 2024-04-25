ff780e0179441b3acd717d1df4022592e02ca902
mosama88 <mosama88@hotmail.com>
2024-04-25 14:06:35 +0200

    services

--- app/Repository/Services/ServiceRepository.php
+++ app/Repository/Services/ServiceRepository.php
@@ -2,11 +2,13 @@
 
 namespace  App\Repository\Services;
 use App\Interfaces\Services\ServiceRepositoryInterface;
+use App\Models\Service;
 
 class ServiceRepository implements ServiceRepositoryInterface
 {
     public function index(){
-       return view ('dashboard.services.single-service-index');
+        $services = Service::all();
+       return view ('dashboard.services.single-service.index', compact('services'));
     }
 
     public function store( $request)
--- resources/views/dashboard/services/single-service/index.blade.php
+++ resources/views/dashboard/services/single-service/index.blade.php
@@ -1,19 +1,15 @@
 @extends('dashboard.layouts.master')
-@section('title', 'الأطباء')
-@section('page-title', 'الأطباء')
+@section('title', 'الخدمات المفرده')
+@section('page-title', 'الخدمات المفرده')
 @section('page-link-back')
     <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">لوحة التحكم</a>
     </li>
 @endsection
-@section('current-page', 'الأطباء')
+@section('current-page', 'الخدمات المفرده')
 @section('content')
     @include('dashboard.layouts.page-link')
     <!--Internal   Notify -->
     <link href="{{ asset('dashboard') }}/assets/plugins/notify/css/notifIt.css" rel="stylesheet" />
-
-    {{-- Start Row --}}
-    @include('dashboard.messages_alert')
-
     <div class="page-content">
 
         <div class="container-fluid">
@@ -23,15 +19,19 @@
                         <div class="col-sm-12 col-md-12 col-xl-12 text-end">
                             <div class="my-4">
                                 <!-- Satic modal -->
-                                <a href="{{ route('dashboard.doctors.create') }}"
-                                    class="btn btn-primary btn-lg waves-effect waves-light">
-                                    أضف دكتور <i class="fas fa-plus-square"></i>
-                                </a>
+                                <button type="button" class="btn btn-primary btn-lg waves-effect waves-light"
+                                        data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                    أضافة خدمه
+                                </button>
+                            </div>
+                            @include('dashboard.services.single-service.add')
+                            <!-- /.modal -->
+                        </div>
                                 <!-- Satic modal -->
                                 <a id="btn_delete_all" class="btn btn-danger btn-lg waves-effect waves-light">
-                                    حذف مجموعة أطباء <i class="fas fa-trash-alt"></i>
+                                    حذف مجموعة خدمات <i class="fas fa-trash-alt"></i>
                                 </a>
-                                @include('dashboard.doctors.delete_select')
+                                {{-- @include('dashboard.doctors.delete_select') --}}
 
                             </div>
                         </div>
@@ -47,55 +47,40 @@
                                     <input class="form-check-input" type="checkbox" value="" name="select_all"
                                         id="example-select-all">
                                     <label class="form-check-label" for="flexCheckDefault">
-                                        أختر الكل
+                                        الكل
                                     </label>
                                 </th>
-                                <th>صورة الطبيب</th>
-                                <th>اسم الطبيب</th>
-                                <th>البريد الالكتروني</th>
-                                <th>الموبايل</th>
-                                <th>القسم</th>
-                                <th>المواعيد</th>
+                                <th>الأسم</th>
+                                <th>سعر الخدمه</th>
                                 <th>الحاله</th>
+                                <th>ملاحظات</th>
+                                <th>تاريخ الأضافه</th>
                                 <th>العمليات</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($doctors as $doctor)
+                            @foreach ($services as $service)
                                 <tr>
                                     <td class="table-primary">{{ $loop->iteration }}</td>
                                     <td class="text-center">
-                                        <input type="checkbox" name="delete_select" value="{{ $doctor->id }}"
+                                        <input type="checkbox" name="delete_select" value="{{ $service->id }}"
                                             class="form-check-input">
                                     </td>
-                                    <td class="text-center">
-                                        @if ($doctor->image)
-                                            <img src="{{ asset('dashboard/assets/images/uploads/doctors/' . $doctor->image->filename) }}"
-                                                class="rounded avatar-md" alt="">
-                                        @else
-                                            <img src="{{ asset('dashboard/assets/images/uploads/doctors/doctor_default.png') }}"
-                                                class="rounded avatar-md" alt="">
-                                        @endif
-                                    </td>
-                                    <td><a href="#">{{ $doctor->name }}</a> </td>
-                                    <td>{{ $doctor->email }}</td>
-                                    <td>{{ $doctor->phone }}</td>
-                                    <td>{{ $doctor->section->name }}</td>
+
+                                    <td><a href="#">{{ $service->name }}</a> </td>
+                                    <td>{{ $service->price }}</td>
                                     <td>
-                                        @foreach ($doctor->doctorappointments as $appointment)
-                                            {{ $appointment->name }}
-                                        @endforeach
-                                    </td>
-                                    <td>
-                                        @if ($doctor->status == 'active')
+                                        @if ($service->status == '1')
                                             <span class="badge bg-success">مفعل</span>
                                         @else
                                             <span class="badge bg-danger">غير مفعل</span>
                                         @endif
                                         <div
-                                            class="dot-label bg-{{ $doctor->status == 'active' ? 'success' : 'danger' }} ml-1">
+                                            class="dot-label bg-{{ $service->status == 'active' ? 'success' : 'danger' }} ml-1">
                                         </div>
-                                    </td>
+                                    </td>                                    <td>{{ Str::limit($service->description, 50) }}</td>
+                                    <td>{{ $service->created_at->diffForHumans() }}</td>
+
                                     <td>
                                         <div class="btn-group dropend">
                                             <button type="button"
@@ -107,23 +92,23 @@
 
                                                 {{-- Change Data --}}
                                                 <a class="dropdown-item"
-                                                    href="{{ route('dashboard.doctors.edit', $doctor->id) }}"><i
+                                                    href="{{ route('dashboard.doctors.edit', $service->id) }}"><i
                                                         style="color: #0ba360"
                                                         class="text-success ti-user"></i>&nbsp;&nbsp;تعديل
                                                     البيانات</a>
 
                                                 {{-- Change Password --}}
                                                 <a class="dropdown-item modal-effect"
-                                                    href="#update_password{{ $doctor->id }}" data-bs-toggle="modal">
+                                                    href="#update_password{{ $service->id }}" data-bs-toggle="modal">
                                                     <i class="text-primary ti-key"></i>&nbsp;&nbsp;تغير كلمة المرور</a>
 
                                                 {{-- Change Status --}}
                                                 <a class="dropdown-item modal-effect"
-                                                    href="#update_status{{ $doctor->id }}" data-bs-toggle="modal"><i
+                                                    href="#update_status{{ $service->id }}" data-bs-toggle="modal"><i
                                                         class="text-warning ti-back-left"></i>&nbsp;&nbsp;تغير الحالة</a>
 
                                                 {{-- Delete Doctor --}}
-                                                <a class="dropdown-item modal-effect" href="#delete{{ $doctor->id }}"
+                                                <a class="dropdown-item modal-effect" href="#delete{{ $service->id }}"
                                                     data-bs-toggle="modal"><i
                                                         class="text-danger ti-trash"></i>&nbsp;&nbsp;حذف
                                                     البيانات</a>
@@ -131,11 +116,11 @@
                                             </div>
                                         </div>
 
-                                        @include('dashboard.doctors.delete')
+                                        {{-- @include('dashboard.doctors.delete') --}}
                                     </td>
-                                    @include('dashboard.doctors.update_status')
+                                    {{-- @include('dashboard.doctors.update_status') --}}
                                 </tr>
-                                @include('dashboard.doctors.update_password')
+                                {{-- @include('dashboard.doctors.update_password') --}}
                             @endforeach
                         </tbody>
                     </table>
@@ -143,8 +128,9 @@
             </div>
         </div>
     </div>
-</div>
-    <!-- End Page-content -->
+    </div>
+
+
 
 
 
