1f19aaba81439d57ae16af174598a23535b05893
mosama88 <mosama88@hotmail.com>
2024-04-25 12:19:43 +0200

    Services4

--- resources/views/dashboard/services/single-service-index.blade.php
+++ resources/views/dashboard/services/single-service-index.blade.php
@@ -1,1 +1,28 @@
-Hello Every Body
+@extends('dashboard.layouts.master')
+@section('title', 'الخدمات المفرده')
+@section('page-title', 'الخدمات المفرده')
+@section('page-link-back')
+    <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">لوحة التحكم</a>
+    </li>
+@endsection
+@section('current-page', 'الخدمات المفرده')
+@section('content')
+    @include('dashboard.layouts.page-link')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @include('dashboard.layouts.scripts')
+
+@endsection
