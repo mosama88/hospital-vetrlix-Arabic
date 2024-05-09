4b3b50f6c82bcc085af5b96f0c4b67d72f924d81
mosama88 <mosama88@hotmail.com>
2024-05-09 22:49:00 +0300

    invoice 6

--- app/Livewire/SingleInvoices.php
+++ app/Livewire/SingleInvoices.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Models\Doctor;
+use App\Models\FundAccount;
 use App\Models\Group;
 use App\Models\Patient;
+use App\Models\PatientAccount;
 use App\Models\Section;
 use App\Models\Service;
 use App\Models\single_invoice;
@@ -53,30 +55,79 @@ class SingleInvoices extends Component
     }
 
     public function store(){
-
-        $single_invoices = new single_invoice();
+        //فى حالة كانت الفاتورة نقدى
+        if($this->type == 1){
+//حفظ فى جدول الفواتير
+            $single_invoices = new single_invoice();
 //                    $single_invoices->invoice_type = 1;
-        $single_invoices->invoice_date = date('Y-m-d');
-        $single_invoices->patient_id = $this->patient_id;
-        $single_invoices->doctor_id = $this->doctor_id;
-        $section = DB::table('sections')->where('name', $this->section_id)->first();
-        if ($section) {
-            $single_invoices->section_id = $section->id;
-        } else {
-            // Handle the case when the section doesn't exist
+            $single_invoices->invoice_date = date('Y-m-d');
+            $single_invoices->patient_id = $this->patient_id;
+            $single_invoices->doctor_id = $this->doctor_id;
+            $section = DB::table('sections')->where('name', $this->section_id)->first();
+            if ($section) {
+                $single_invoices->section_id = $section->id;
+            } else {
+                // Handle the case when the section doesn't exist
+            }
+            $single_invoices->Service_id = $this->Service_id;
+            $single_invoices->price = $this->price;
+            $single_invoices->discount_value = $this->discount_value;
+            $single_invoices->tax_rate = $this->tax_rate;
+            // قيمة الضريبة = السعر - الخصم * نسبة الضريبة /100
+            $single_invoices->tax_value = ($this->price -$this->discount_value) * ((is_numeric($this->tax_rate) ? $this->tax_rate : 0) / 100);
+            // الاجمالي شامل الضريبة  = السعر - الخصم + قيمة الضريبة
+            $single_invoices->total_with_tax = $single_invoices->price -  $single_invoices->discount_value + $single_invoices->tax_value;
+            $single_invoices->type = $this->type;
+            $single_invoices->save();
+
+            //حفظ فى جدول الصندوق
+            $fund_accounts = new FundAccount();
+            $fund_accounts->date = date('Y-m-d');
+            $fund_accounts->single_invoice_id = $single_invoices->id;
+            $fund_accounts->Debit = $single_invoices->total_with_tax;
+            $fund_accounts->credit = 0.00;
+            $fund_accounts->save();
+
+
+            $this->InvoiceUpdated =true;
+            $this->show_table =true;
+
+        }else{
+
+            $single_invoices = new single_invoice();
+            $single_invoices->invoice_date = date('Y-m-d');
+            $single_invoices->patient_id = $this->patient_id;
+            $single_invoices->doctor_id = $this->doctor_id;
+            $section = DB::table('sections')->where('name', $this->section_id)->first();
+            if ($section) {
+                $single_invoices->section_id = $section->id;
+            } else {
+                // Handle the case when the section doesn't exist
+            }
+            $single_invoices->Service_id = $this->Service_id;
+            $single_invoices->price = $this->price;
+            $single_invoices->discount_value = $this->discount_value;
+            $single_invoices->tax_rate = $this->tax_rate;
+            // قيمة الضريبة = السعر - الخصم * نسبة الضريبة /100
+            $single_invoices->tax_value = ($this->price -$this->discount_value) * ((is_numeric($this->tax_rate) ? $this->tax_rate : 0) / 100);
+            // الاجمالي شامل الضريبة  = السعر - الخصم + قيمة الضريبة
+            $single_invoices->total_with_tax = $single_invoices->price -  $single_invoices->discount_value + $single_invoices->tax_value;
+            $single_invoices->type = $this->type;
+            $single_invoices->save();
+
+            //حفظ فى جدول حسابات المريض
+            $patient_accounts = new PatientAccount();
+            $patient_accounts->date = date('Y-m-d');
+            $patient_accounts->single_invoice_id = $single_invoices->id;
+            $patient_accounts->patient_id = $single_invoices->patient_id;
+            $patient_accounts->Debit = $single_invoices->total_with_tax;
+            $patient_accounts->credit = 0.00;
+            $patient_accounts->save();
+            $this->InvoiceUpdated =true;
+            $this->show_table =true;
         }
-        $single_invoices->Service_id = $this->Service_id;
-        $single_invoices->price = $this->price;
-        $single_invoices->discount_value = $this->discount_value;
-        $single_invoices->tax_rate = $this->tax_rate;
-        // قيمة الضريبة = السعر - الخصم * نسبة الضريبة /100
-        $single_invoices->tax_value = ($this->price -$this->discount_value) * ((is_numeric($this->tax_rate) ? $this->tax_rate : 0) / 100);
-        // الاجمالي شامل الضريبة  = السعر - الخصم + قيمة الضريبة
-        $single_invoices->total_with_tax = $single_invoices->price -  $single_invoices->discount_value + $single_invoices->tax_value;
-        $single_invoices->type = $this->type;
-        $single_invoices->save();
-        $this->InvoiceUpdated =true;
-        $this->show_table =true;
+
+
 
     }
 }
---
+++ database/migrations/2024_05_09_190110_create_fund_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\single_invoice;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fund_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->foreignId('single_invoice_id')->nullable()->references('id')->on('single_invoices')->onDelete('cascade');
+            $table->decimal('Debit',8,2)->nullable(); //مدين
+            $table->decimal('credit',8,2)->nullable();  //دائن
+//          $table->foreignId('receipt_id')->nullable()->references('id')->on('receipt_accounts')->onDelete('cascade');
+//          $table->foreignId('Payment_id')->nullable()->references('id')->on('payment_accounts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fund_accounts');
+    }
+};
