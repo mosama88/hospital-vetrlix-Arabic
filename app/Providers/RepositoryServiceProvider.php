8485e147f865a664a2c139d31ba977882f73335b
mosama88 <mosama88@hotmail.com>
2024-05-10 19:05:00 +0300

    recipite

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Interfaces\Ambulances\AmbulanceRepositoryInterface;
+use App\Interfaces\Finance\ReceiptRepositoryInterface;
 use App\Interfaces\Patients\PatientRepositoryInterface;
 use App\Interfaces\Insurances\InsuranceRepositoryInterface;
 use App\Repository\Ambulances\AmbulanceRepository;
+use App\Repository\Finance\ReceiptRepository;
 use App\Repository\Insurances\InsuranceRepository;
 use App\Repository\Patients\PatientRepository;
 use Illuminate\Support\ServiceProvider;
@@ -29,6 +31,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(InsuranceRepositoryInterface::class, InsuranceRepository::class);
         $this->app->bind(AmbulanceRepositoryInterface::class, AmbulanceRepository::class);
         $this->app->bind(PatientRepositoryInterface::class, PatientRepository::class);
+        $this->app->bind(ReceiptRepositoryInterface::class, ReceiptRepository::class);
 
 
     }
